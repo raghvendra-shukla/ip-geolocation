99676492e37efa33fddb7cfacf98200f71233dce
Raghvendra <rannyshukla762@gmail.com>
2023-01-07 17:36:35 +0530

    2nd commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,22 +4,32 @@ import Mapp from './Mapp';
 function Home() {
   const [data, setdata] = useState([]);
   const response=async()=>{
-    const url=`https://api.bigdatacloud.net/data/ip-geolocation-full?ip=49.36.25.63&localityLanguage=en&key=bdc_891fe2c43e0a44dea6eddf797d5c372b`;
-    const ferchData=await fetch(url);
-    const parsedData=await ferchData.json();
-    setdata(parsedData);
+    const url=`https://api.bigdatacloud.net/data/ip-geolocation-full?ip=8.8.8.8&localityLanguage=en&key=bdc_891fe2c43e0a44dea6eddf797d5c372b`;
+    const fetchData=await fetch(url);
+    const parsedData=await fetchData.json();
+    setdata(parsedData.location);
+    // console.log("yes");
   }
 
   useEffect(() => {
     response();
+    // console.log("yes1");
   }, [])
   
   return (
     <>
-     <div>Home</div>
-     <div className="conatiner">
-        <Mapp/>
-     </div>
+    <div className="container">
+      <div className="container my-2">
+          <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Enter the IP address to locate" aria-label="Search"/>
+              <button className="btn btn-dark text-white" type="submit">Search</button>
+            </form>
+      </div>
+      <div className="conatiner my-2 text-center">
+          <Mapp lati={data.latitude} loni={data.longitude}/>
+          {/* {console.log(typeof(data.latitude))} */}
+      </div>
+    </div>
     </>
   )
 }
--- src/Components/Mapp.js
+++ src/Components/Mapp.js
@@ -8,25 +8,39 @@ import Map, { GeolocateControl, NavigationControl, Marker } from "react-map-gl";
 // mapboxgl.accessToken =
 //   "pk.eyJ1IjoiaGFycnkxMjM0OTgiLCJhIjoiY2s4OXh1c3BqMGFsZzNvbXA3YmYyaGFhYSJ9.wmVMiMxlSqpzJPsj-UXr3Q";
 
-function Mapp() {
+function Mapp(props) {
+  const {lati,loni}=props;
+  // console.log(typeof(lati));
+  // console.log(lati);
+  // console.log(loni);
   {
     document.body.style.backgroundColor = "#b7acac";
   }
+  // marker.setLatLng([lat, lon]);
+  // mymap.setView([lat, lon], 5);
+  // const ll = new mapboxgl.LngLat(lati, loni);
+  const ll = new mapboxgl.LngLat(72.877656, 19.075984);
+  ll.toBounds(100).toArray();
+  // console.log(ll.lng); // = -123.9749
   const [viewport, setviewport] = useState({
-    longitude: 72.877656,
-    latitude: 19.075984,
+    // longitude: 72.877656,
+    // latitude: 19.075984,
+    latitude: parseFloat(lati),
+    longitude: parseFloat(loni),
     zoom: 12,
   });
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((pos) => {
       setviewport({
         ...viewport,
-        latitude: pos.coords.latitude,
-        longitude: pos.coords.longitude,
+        // latitude: pos.coords.latitude,
+        // longitude: pos.coords.longitude,
+        latitude: parseFloat(lati),
+        longitude: parseFloat(loni),
         zoom: 12,
       });
     });
-  }, [viewport]);
+  }, []);
   mapboxgl.accessToken =
     "pk.eyJ1IjoiaGFycnkxMjM0OTgiLCJhIjoiY2s4OXh1c3BqMGFsZzNvbXA3YmYyaGFhYSJ9.wmVMiMxlSqpzJPsj-UXr3Q";
   return (
@@ -58,6 +72,11 @@ function Mapp() {
       </div>
     </>
   );
+}
+
+Mapp.defaultProps={
+  lati:19.075984,
+  loni:72.877656
 }
 
 export default Mapp;
