1619e4b27478bb8a2d6b1e04f6097fb9a501cc30
Raghvendra <rannyshukla762@gmail.com>
2023-01-09 17:45:34 +0530

    adding search functionality

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,26 +3,39 @@ import Mapp from './Mapp';
 
 function Home() {
   const [data, setdata] = useState([]);
+  const [ip,setIp]=useState("8.8.8.8");
   const response=async()=>{
-    const url=`https://api.bigdatacloud.net/data/ip-geolocation-full?ip=8.8.8.8&localityLanguage=en&key=bdc_891fe2c43e0a44dea6eddf797d5c372b`;
+    const url=`https://api.bigdatacloud.net/data/ip-geolocation-full?ip=${ip}&localityLanguage=en&key=bdc_891fe2c43e0a44dea6eddf797d5c372b`;
     const fetchData=await fetch(url);
     const parsedData=await fetchData.json();
     setdata(parsedData.location);
     // console.log("yes");
   }
 
+  const handleonChange=(e)=>{
+    localStorage.setItem("ip",e.target.value);
+  }
+
+  const handleonClick=(e)=>{
+    e.preventDefault();
+    const Ip=localStorage.getItem("ip");
+    // console.log(Ip);
+    // console.log(typeof(Ip));
+    setIp(Ip);
+  }
+
   useEffect(() => {
     response();
     // console.log("yes1");
-  }, [])
+  }, [ip])
   
   return (
     <>
     <div className="container">
       <div className="container my-2">
           <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Enter the IP address to locate" aria-label="Search"/>
-              <button className="btn btn-dark text-white" type="submit">Search</button>
+              <input className="form-control me-2" type="search" placeholder="Enter the IP address to locate" aria-label="Search" onChange={handleonChange}/>
+              <button className="btn btn-dark text-white" type="submit" onClick={handleonClick}>Search</button>
             </form>
       </div>
       <div className="conatiner my-2 text-center">
--- src/Components/Mapp.js
+++ src/Components/Mapp.js
@@ -3,44 +3,28 @@ import { useRef, useEffect, useState } from "react";
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import mapboxgl from "mapbox-gl";
 // import { Marker } from 'react-map-gl';
-import Map, { GeolocateControl, NavigationControl, Marker } from "react-map-gl";
+import Map, { GeolocateControl, NavigationControl, Marker ,FullscreenControl} from "react-map-gl";
 
 // mapboxgl.accessToken =
 //   "pk.eyJ1IjoiaGFycnkxMjM0OTgiLCJhIjoiY2s4OXh1c3BqMGFsZzNvbXA3YmYyaGFhYSJ9.wmVMiMxlSqpzJPsj-UXr3Q";
 
 function Mapp(props) {
   const {lati,loni}=props;
-  // console.log(typeof(lati));
+  const [lat, setlat] = useState(props.lati);
+  const [lon, setlon] = useState(props.loni);
+  // console.log(lat);
+  // console.log(lon);
   // console.log(lati);
   // console.log(loni);
   {
     document.body.style.backgroundColor = "#b7acac";
   }
-  // marker.setLatLng([lat, lon]);
-  // mymap.setView([lat, lon], 5);
-  // const ll = new mapboxgl.LngLat(lati, loni);
-  const ll = new mapboxgl.LngLat(72.877656, 19.075984);
-  ll.toBounds(100).toArray();
-  // console.log(ll.lng); // = -123.9749
-  const [viewport, setviewport] = useState({
-    // longitude: 72.877656,
-    // latitude: 19.075984,
-    latitude: parseFloat(lati),
-    longitude: parseFloat(loni),
-    zoom: 12,
-  });
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition((pos) => {
-      setviewport({
-        ...viewport,
-        // latitude: pos.coords.latitude,
-        // longitude: pos.coords.longitude,
-        latitude: parseFloat(lati),
-        longitude: parseFloat(loni),
-        zoom: 12,
-      });
+    navigator.geolocation.getCurrentPosition(() => {
+      setlat(lati);
+      setlon(loni);
     });
-  }, []);
+  },);
   mapboxgl.accessToken =
     "pk.eyJ1IjoiaGFycnkxMjM0OTgiLCJhIjoiY2s4OXh1c3BqMGFsZzNvbXA3YmYyaGFhYSJ9.wmVMiMxlSqpzJPsj-UXr3Q";
   return (
@@ -50,22 +34,28 @@ function Mapp(props) {
           <Map
             mapStyle="mapbox://styles/mapbox/streets-v12"
             style={{ width: "80vw", height: "80vh" }}
-            initialViewState={viewport}
-            onViewportChange={(viewport) => {
-              setviewport(viewport);
-              // mapboxAccessToken={process.env.TOKEN }
+            initialViewState={{
+              latitude:lat,
+              longitude:lon,
+              zoom:5
             }}
+            // onViewportChange={(lat,lon) => {
+            //   setlat(lati);
+            //   setlon(loni);
+            //   // mapboxAccessToken={process.env.TOKEN }
+            // }}
           >
             <Marker
               style={{color:'black'}}
-              longitude={viewport.longitude}
-              latitude={viewport.latitude}
+              longitude={lon}
+              latitude={lat}
             />
             <GeolocateControl
               // mapboxAccessToken={token}
               positionOptions={{ enableHighAccuracy: true }}
               trackUserLocation={true}
             />
+            <FullscreenControl/>
             <NavigationControl position="bottom-right" />
           </Map>
         </div>
